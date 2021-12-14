fddc7aca4042065d355ceb7ba2303d60ef90b93e
Nathan Swain <nswain@aquaveo.com>
2021-12-14 11:21:20 -0700

    Datatable and table examples updated.

--- tethysapp/gizmo_showcase/controllers/tables.py
+++ tethysapp/gizmo_showcase/controllers/tables.py
@@ -19,6 +19,18 @@ def table_view(request):
         bordered=False,
         condensed=False
     )
+    
+    dark_table_view = TableView(
+        column_names=('Name', 'Age', 'Job'),
+        rows=[('Bill', 30, 'contractor'),
+                ('Fred', 18, 'programmer'),
+                ('Bob', 26, 'boss')],
+        hover=True,
+        striped=True,
+        bordered=True,
+        condensed=False,
+        dark=True
+    )
 
     table_view_edit = TableView(
         column_names=('Name', 'Age', 'Job'),
@@ -36,6 +48,7 @@ def table_view(request):
     context = {
         'docs_endpoint': docs_endpoint,
         'table_view': table_view,
+        'dark_table_view': dark_table_view,
         'table_view_edit': table_view_edit,
     }
     return render(request, 'gizmo_showcase/table_view.html', context)
@@ -55,6 +68,19 @@ def datatable_view(request):
         orderClasses=False,
         lengthMenu=[[10, 25, 50, -1], [10, 25, 50, "All"]],
     )
+    
+    datatable_dark = DataTableView(
+        column_names=('Name', 'Age', 'Job'),
+        rows=[('Bill', 30, 'contractor'),
+            ('Fred', 18, 'programmer'),
+            ('Bob', 26, 'boss')],
+        searching=False,
+        orderClasses=False,
+        lengthMenu=[[10, 25, 50, -1], [10, 25, 50, "All"]],
+        hover=True,
+        striped=True,
+        dark=True,
+    )
 
     datatable_with_extension = DataTableView(
         column_names=('Name', 'Age', 'Job'),
@@ -67,6 +93,7 @@ def datatable_view(request):
     context = {
         'docs_endpoint': docs_endpoint,
         'datatable_default': datatable_default,
+        'datatable_dark': datatable_dark,
         'datatable_with_extension': datatable_with_extension,
     }
     return render(request, 'gizmo_showcase/datatable_view.html', context)
