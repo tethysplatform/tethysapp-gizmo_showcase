de1ade4f0d3e8081a213345ca11832b92483ae7c
Nathan Swain <nswain@aquaveo.com>
2021-12-15 12:52:29 -0700

    MapView example functioning and updated.

--- tethysapp/gizmo_showcase/controllers/map_view.py
+++ tethysapp/gizmo_showcase/controllers/map_view.py
@@ -1,5 +1,7 @@
+import json
 from django.shortcuts import render
 from django.contrib import messages
+import geojson
 from tethys_sdk.permissions import login_required
 from tethys_sdk.gizmos import MapView, MVView, MVDraw, MVLayer, MVLegendClass
 from .common import docs_endpoint
@@ -97,53 +99,61 @@ def map_view(request):
         }},
     }
 
-    geojson_layer = MVLayer(source='GeoJSON',
-                            options=geojson_object,
-                            layer_options={'style_map': style_map},
-                            legend_title='Test GeoJSON',
-                            legend_extent=[-46.7, -48.5, 74, 59],
-                            legend_classes=[
-                                MVLegendClass('polygon', 'Polygons', fill='rgba(0, 0, 255, 0.1)', stroke='blue'),
-                                MVLegendClass('line', 'Lines', stroke='green'),
-                                MVLegendClass('point', 'Points', fill='red')
-                            ])
+    geojson_layer = MVLayer(
+        source='GeoJSON',
+        options=geojson_object,
+        layer_options={'style_map': style_map},
+        legend_title='Test GeoJSON',
+        legend_extent=[-46.7, -48.5, 74, 59],
+        legend_classes=[
+            MVLegendClass('polygon', 'Polygons', fill='rgba(0, 0, 255, 0.1)', stroke='blue'),
+            MVLegendClass('line', 'Lines', stroke='green'),
+            MVLegendClass('point', 'Points', fill='red')
+        ]
+    )
 
     map_layers.append(geojson_layer)
 
     if get_geoserver_wms():
         # Define GeoServer Layer
-        geoserver_layer = MVLayer(source='ImageWMS',
-                                  options={'url': get_geoserver_wms(),
-                                           'params': {'LAYERS': 'topp:states'},
-                                           'serverType': 'geoserver'},
-                                  legend_title='USA Population',
-                                  legend_extent=[-126, 24.5, -66.2, 49],
-                                  legend_classes=[
-                                      MVLegendClass('polygon', 'Low Density', fill='#00ff00', stroke='#000000'),
-                                      MVLegendClass('polygon', 'Medium Density', fill='#ff0000', stroke='#000000'),
-                                      MVLegendClass('polygon', 'High Density', fill='#0000ff', stroke='#000000')
-                                  ])
+        geoserver_layer = MVLayer(
+            source='ImageWMS',
+            options={'url': get_geoserver_wms(),
+                    'params': {'LAYERS': 'topp:states'},
+                    'serverType': 'geoserver'},
+            legend_title='USA Population',
+            legend_extent=[-126, 24.5, -66.2, 49],
+            legend_classes=[
+                MVLegendClass('polygon', 'Low Density', fill='#00ff00', stroke='#000000'),
+                MVLegendClass('polygon', 'Medium Density', fill='#ff0000', stroke='#000000'),
+                MVLegendClass('polygon', 'High Density', fill='#0000ff', stroke='#000000')
+            ]
+        )
 
-        map_layers.append(geoserver_layer)
+        # map_layers.append(geoserver_layer)
 
     # Define KML Layer
-    kml_layer = MVLayer(source='KML',
-                        options={'url': '/static/tethys_gizmos/data/model.kml'},
-                        legend_title='Park City Watershed',
-                        legend_extent=[-111.60, 40.57, -111.43, 40.70],
-                        legend_classes=[
-                            MVLegendClass('polygon', 'Watershed Boundary', fill='#ff8000'),
-                            MVLegendClass('line', 'Stream Network', stroke='#0000ff'),
-                        ])
+    kml_layer = MVLayer(
+        source='KML',
+        options={'url': '/static/gizmo_showcase/data/model.kml'},
+        legend_title='Park City Watershed',
+        legend_extent=[-111.60, 40.57, -111.43, 40.70],
+        legend_classes=[
+            MVLegendClass('polygon', 'Watershed Boundary', fill='#ff8000'),
+            MVLegendClass('line', 'Stream Network', stroke='#0000ff'),
+        ]
+    )
 
     map_layers.append(kml_layer)
 
     # Tiled ArcGIS REST Layer
-    arc_gis_layer = MVLayer(source='TileArcGISRest',
-                            options={'url': 'http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/' +
-                                            'Specialty/ESRI_StateCityHighway_USA/MapServer'},
-                            legend_title='ESRI USA Highway',
-                            legend_extent=[-173, 17, -65, 72])
+    arc_gis_layer = MVLayer(
+        source='TileArcGISRest',
+        options={'url': 'http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/' +
+                        'Specialty/ESRI_StateCityHighway_USA/MapServer'},
+        legend_title='ESRI USA Highway',
+        legend_extent=[-173, 17, -65, 72]
+    )
 
     map_layers.append(arc_gis_layer)
 
@@ -161,13 +171,37 @@ def map_view(request):
         legend=True
     )
 
+    # Get the geometry drawn by the user
     submitted_geometry = request.POST.get('geometry', None)
+    
+    # Convert GeometryCollection into a FeatureCollection if given
+    if submitted_geometry:
+        geojson_objs = geojson.loads(submitted_geometry)
+        
+        # Create a Feature for each geometry
+        features = []
+        for geometry in geojson_objs.geometries:
+            properties = geometry.pop('properties', [])
+            features.append({
+                'type': 'Feature',
+                'geometry': geometry,
+                'properties': properties
+            })
 
-    if submitted_geometry is not None:
-        messages.info(request, submitted_geometry)
+        # Create FeatureCollection wrapper with list of features
+        feature_collection = {
+            'type': 'FeatureCollection',
+            'features': features
+        }
+        
+        # Set initial features on drawing layer (as geojson string)
+        drawing_options.initial_features = json.dumps(feature_collection)
+        
+        # Log in alert message
+        messages.success(request, "Geometry added to the map successfully.")
 
     context = {
         'docs_endpoint': docs_endpoint,
         'map_view': map_view_options
-        }
+    }
     return render(request, 'gizmo_showcase/map_view.html', context)
