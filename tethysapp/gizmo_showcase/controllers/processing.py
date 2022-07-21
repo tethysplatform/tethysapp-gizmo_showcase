8afb1bea4af6d48bfbb9512a2a0dd8a60ed7ef22
sdc50 <sdc50@byu.net>
2022-07-21 15:40:20 -0600

    Custom job actions (#2)
    * custom job actions
    
    * extended properties
    
    * fix job creation
    
    * add custom_job_action

--- tethysapp/gizmo_showcase/controllers/processing.py
+++ tethysapp/gizmo_showcase/controllers/processing.py
@@ -1,3 +1,4 @@
+from time import sleep
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from tethys_sdk.routing import controller
@@ -16,7 +17,7 @@ def jobs_table(request):
     # Table View
     jobs_table_options = JobsTable(
         jobs=jobs,
-        column_fields=('id', 'name', 'description', 'creation_time'),
+        column_fields=('id', 'name', 'description', ('Created At', 'creation_time'), 'extended_properties.parity'),
         hover=True,
         striped=False,
         bordered=False,
@@ -24,10 +25,26 @@ def jobs_table(request):
         monitor_url='gizmo_showcase:jobs_table_results',
         results_url='gizmo_showcase:jobs_table_results',
         refresh_interval=10000,
-        run_btn=True,
-        delete_btn=True,
         show_detailed_status=True,
-        actions=['run', 'resubmit', 'log', 'monitor', 'results', 'terminate', 'delete'],
+        delay_loading_status=True,
+        actions=[
+            'run', 'pause', 'resume', 'resubmit', '|', 'logs', 'monitor', 'results', '|', 'terminate', 'delete', '|',
+            dict(
+                label='Custom Action',
+                callback_or_url=custom_action,
+                enabled_callback=lambda self, job_status: self.extended_properties['parity'] == 'even',
+                confirmation_message='Custom actions run user-defined custom code. '
+                    'This custom action will sleep for 2 seconds and then return. </br></br>'
+                    'Custom action can also have customized code to enable/disable the action '
+                    '(e.g. this action is only enabled on jobs that have "even" as the extended property "parity"). '
+                    '</br></br>'
+                    'Additionally, you can specify whether to show the loading overlay when the action is performed. '
+                    'This action has the overlay enabled. </br></br>'
+             
+                    'Are you sure you want to perform a custom action?',
+                show_overlay=True
+            ),
+        ],
     )
 
     context = {
@@ -50,6 +67,7 @@ def jobs_table_results(request, job_id):
 def create_sample_jobs(request):
     """Controller that creates sample Jobs for the Jobs Table demo."""
     from tethys_compute.models import BasicJob, CondorWorkflow
+
     def create_job(job_id, description, status, status_msg=None, workflow=False):
         Job = CondorWorkflow if workflow else BasicJob
         job = Job(
@@ -59,6 +77,7 @@ def create_sample_jobs(request):
             label='gizmos_showcase',
             status_message=status_msg,
             _status=status,
+            extended_properties={'parity': 'even' if job_id % 2 == 0 else 'odd'}
         )
         job.save()
 
@@ -73,8 +92,13 @@ def create_sample_jobs(request):
         create_job(i, desc, status)
         create_job(i + 10, desc, status, status_msg=f'{desc} status message')
 
-    create_job('20', 'Running multi-process job with various statuses', 'VAR')
-    create_job('21', 'Completed multi-process job with some errors', 'VCP')
-    create_job('22', 'Workflow job with multiple nodes.', 'VAR', workflow=True)
+    create_job(20, 'Running multi-process job with various statuses', 'VAR')
+    create_job(21, 'Completed multi-process job with some errors', 'VCP')
+    create_job(22, 'Workflow job with multiple nodes.', 'VAR', workflow=True)
 
     return redirect(reverse('gizmo_showcase:jobs_table'))
+
+
+def custom_action(self):
+    sleep(2)
+    self.status = 'My Custom Status'
